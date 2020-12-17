4d7f2d5a5b0e60c63133555653d80fbc9b7c82f0
Felix <>
2020-12-17 21:10:58 +0100

    working comit

--- main.ts
+++ main.ts
@@ -1,3 +1,49 @@
 import iro from '@jaames/iro';
+import ColorButton from './ColorButton';
 
-const colorPicker = new (iro.ColorPicker as any)('#picker');
+let buttons: ColorButton[] = [];
+const hexColorInput = document.getElementById('hexColorInput') as HTMLInputElement;
+const buttonArea = document.getElementById('buttonArea');
+const storageValue = localStorage.getItem('buttons');
+
+//event listener
+const colorPicker = new (iro.ColorPicker as any)('#picker', {
+    width: 500
+});
+
+document.getElementById('add').onclick = () => {
+    buttons.push(new ColorButton('#e63232', changeColorEvent, buttonArea));
+    saveToLocalStorage();
+};
+
+colorPicker.on('color:change', () => {
+    buttons.find(b => b.selected).color = colorPicker.color.hexString;
+    hexColorInput.value = colorPicker.color.hexString.substring(1);
+    saveToLocalStorage();
+});
+
+//helpers
+function changeColorEvent(color: string) {
+    colorPicker.color.hexString = color;
+    saveToLocalStorage();
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('buttons', JSON.stringify(buttons.map(b => b.color)));
+}
+
+
+//initialization
+
+if (storageValue !== undefined && storageValue !== '""') {
+    JSON.parse(storageValue).forEach(colorCode => {
+        buttons.push(new ColorButton(colorCode, changeColorEvent, buttonArea));
+    });
+} else {
+    buttons = [new ColorButton('#4287f5', changeColorEvent, buttonArea),
+    new ColorButton('#ff1c68', changeColorEvent, buttonArea),
+    new ColorButton('#39ff0d', changeColorEvent, buttonArea)
+    ];
+}
+
+buttons[0].select();
